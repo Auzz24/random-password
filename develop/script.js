7dea56bee7f858e3a0545d2ece78f97ec2174373
Austin Miller <austin.miller1424@gmail.com>
2021-12-03 14:21:21 -0600

    added all the function variations

--- develop/script.js
+++ develop/script.js
@@ -1,97 +1,221 @@
 
-var passwordNumber= window.prompt("How many total charachters do you want in your password?");
+var passwordLength= window.prompt("How many total charachters do you want in your password?");
 //console.log(passwordNumber);
-if (passwordNumber < 8 || passwordNumber > 128) {
+
+var passwordLower= window.confirm("Do you want to add a lowercase letter?");
+//console.log(passwordlower);
+
+var passwordCapital= window.confirm("Do you want to add a capital letter?");
+//console.log(passwordCapital);
+
+var passwordNumber= window.confirm("Do you want to add a number into your passowrd?");
+//console.log(passwordNumber);
+
+var passwordSpecial= window.confirm("Do you want to add a special charachter?");
+//console.log(passwordSpecial);
+
+if (passwordLength < 8 || passwordLength > 128) {
   window.alert("Please select a number between 8-128 charachters.");
 } //reload page???
-else { 
-const characters ='abcdefghijklmnopqrstuvwxyz';
 
-function generateString(length) {
+
+if (passwordLower && passwordCapital && passwordNumber && passwordSpecial){
+  
+var characters ='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*()';
+function lowerCapitalNumberSpecialString(length) {
+  let result = ' ';
+  var charactersLength = characters.length;
+  for ( let i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+console.log(lowerCapitalNumberSpecialString(passwordLength));
+}
+
+
+if (passwordLower && passwordCapital && passwordNumber){
+   
+var characters ='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789';
+function lowerCapitalNumberString(length) {
+  let result = ' ';
+  var charactersLength = characters.length;
+  for ( let i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+console.log(lowerCapitalNumberString(passwordLength));
+}
+
+
+if (passwordLower){
+  
+var characters ='abcdefghijklmnopqrstuvwxyz';
+function lowerString(length) {
+  let result = ' ';
+  var charactersLength = characters.length;
+  for ( let i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+console.log(lowerString(passwordLength));
+}
+
+if (passwordLower && passwordCapital){
+  
+var characters ='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+function lowerCapitalString(length) {
+  let result = ' ';
+  var charactersLength = characters.length;
+  for ( let i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+console.log(lowerCapitalString(passwordLength));
+}
+
+if (passwordLower && passwordSpecial){
+  
+  var characters ='abcdefghijklmnopqrstuvwxyz!@#$%^&*()';
+  function lowerSpecialString(length) {
     let result = ' ';
-    const charactersLength = characters.length;
+    var charactersLength = characters.length;
     for ( let i = 0; i < length; i++ ) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-
     return result;
   }
-};
+  console.log(lowerSpecialString(passwordLength));
+  }
 
-console.log(generateString(passwordNumber));
-//};
-
-
-//const charachters = "abcdefghijklmnopqrstuvwxyz"
-
-//function generateString(length){
-  //let result = '';
-  //const charachtersLength = charachters.length;
-  //for (let i=0; i<length; i*passwordNumber){
- //   //result += charachters.charAt(math.floor(Math.random()* charachtersLength));
-  //}
-
-  //return result;
-
-//}
+  if (passwordLower && passwordNumber){
   
-  //console.log(generateString(5));
+    var characters ='abcdefghijklmnopqrstuvwxyz123456789';
+    function lowerNumberString(length) {
+      let result = ' ';
+      var charactersLength = characters.length;
+      for ( let i = 0; i < length; i++ ) {
+          result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+      return result;
+    }
+    console.log(lowerNumberString(passwordLength));
+    }
 
+    if (passwordLower && passwordNumber && passwordSpecial){
+  
+      var characters ='abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()';
+      function lowerNumberSpecialString(length) {
+        let result = ' ';
+        var charactersLength = characters.length;
+        for ( let i = 0; i < length; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+      }
+      console.log(lowerNumberSpecialString(passwordLength));
+      }
 
-//go onto passwordCapital 
+      if (passwordLower && passwordCapital && passwordSpecial){
+  
+        var characters ='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY!@#$%^&*()';
+        function lowerCapitalNumberSpecialString(length) {
+          let result = ' ';
+          var charactersLength = characters.length;
+          for ( let i = 0; i < length; i++ ) {
+              result += characters.charAt(Math.floor(Math.random() * charactersLength));
+          }
+          return result;
+        }
+        console.log(lowerCapitalNumberSpecialString(passwordLength));
+        }
 
-//var passwordCapital= window.confirm("Do you want to add a capital letter?");
-//console.log(passwordCapital);
-//if (){
-  //add a captial letter to the randomPassword varible; 
-//}
-//else(){
-//dont)
- //}
+        if ( passwordCapital ){
+  
+          var characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+          function capitalString(length) {
+            let result = ' ';
+            var charactersLength = characters.length;
+            for ( let i = 0; i < length; i++ ) {
+                result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            }
+            return result;
+          }
+          console.log(capitalString(passwordLength));
+          }
 
+          if (passwordCapital && passwordNumber){
+  
+            var characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789';
+            function capitalNumberString(length) {
+              let result = ' ';
+              var charactersLength = characters.length;
+              for ( let i = 0; i < length; i++ ) {
+                  result += characters.charAt(Math.floor(Math.random() * charactersLength));
+              }
+              return result;
+            }
+            console.log(capitalNumberString(passwordLength));
+            }
 
- // go to next question passwordNumber
+            if (passwordCapital && passwordNumber && passwordSpecial){
+  
+              var characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*()';
+              function capitalNumberSpecialString(length) {
+                let result = ' ';
+                var charactersLength = characters.length;
+                for ( let i = 0; i < length; i++ ) {
+                    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+                }
+                return result;
+              }
+              console.log(capitalNumberSpecialString(passwordLength));
+              }
 
-//var passwordNumber= window.confirm("Do you want to add a number into your passowrd?");
-//console.log(passwordNumber);
-//if (){
-  //add a captial letter to the randomPassword varible; 
-//}
-//else(){
-//dont)
-// }
+              if (passwordNumber){
+  
+                var characters ='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*()';
+                function numberString(length) {
+                  let result = ' ';
+                  var charactersLength = characters.length;
+                  for ( let i = 0; i < length; i++ ) {
+                      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+                  }
+                  return result;
+                }
+                console.log(numberString(passwordLength));
+                }
 
+                if (passwordNumber && passwordSpecial){
+  
+                  var characters ='123456789!@#$%^&*()';
+                  function numberSpecialString(length) {
+                    let result = ' ';
+                    var charactersLength = characters.length;
+                    for ( let i = 0; i < length; i++ ) {
+                        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+                    }
+                    return result;
+                  }
+                  console.log(lowerCapitalNumberSpecialString(passwordLength));
+                  }
 
-// go to passwordSpecial
-
-//var passwordSpecial= window.confirm("Do you want to add a special charachter?");
-//console.log(passwordSpecial);
-//if (){
-  //add a captial letter to the randomPassword varible; 
-//}
-//else(){
-//dont)
-// }
-
-
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
+                  if (passwordSpecial){
+  
+                    var characters ='!@#$%^&*()';
+                    function specialString(length) {
+                      let result = ' ';
+                      var charactersLength = characters.length;
+                      for ( let i = 0; i < length; i++ ) {
+                          result += characters.charAt(Math.floor(Math.random() * charactersLength));
+                      }
+                      return result;
+                    }
+                    console.log(specialString(passwordLength));
+                    }
 //length()
 
 
